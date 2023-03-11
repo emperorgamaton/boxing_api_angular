f7bc70e3967e52e328d717bfe1f9bec2883d1fb0
Tendayi T Makwara <tendayimakwara@gmail.com>
2023-03-11 19:12:06 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,8 @@ export class AppComponent {
     const m: Moment = event.value;
     if(m){
       //let selectedDate = moment(m);
-	  var today = new Date();
-	  let diffInDays = moment(m).diff(moment(today), 'days');
+	    var today = new Date();
+	    let diffInDays = moment(m).diff(moment(today), 'days');
       console.log(diffInDays);
     }
   } 
@@ -36,64 +36,67 @@ export class AppComponent {
 	  })
 	  .subscribe(data => {
 		  console.log(data);
-	  });
-  }
-  
-  
-  title = 'boxing_api';
-  for (var g: number = 0; g < data['DATA'].length; g++) {
-	  for (let i = 0; i < data['DATA'][g]['EVENTS'].length; i++) {
-		  
-		  var homeFighterPictureData = data['DATA'][g]['EVENTS'][i]['HOME_IMAGES'][0];
-		  var awayFighterPictureData = data['DATA'][g]['EVENTS'][i]['AWAY_IMAGES'][0];
-		  
-		  var div = document.createElement("div");
+      for (let g = 0; g < data['DATA'].length; g++) {
+        for (let i = 0; i < data['DATA'][g]['EVENTS'].length; i++) {
+          
+          var homeFighterPictureData = data['DATA'][g]['EVENTS'][i]['HOME_IMAGES'][0];
+          var awayFighterPictureData = data['DATA'][g]['EVENTS'][i]['AWAY_IMAGES'][0];
+          
+          var div = document.createElement("div");
           div.classList.add("fightdiv");
           document.body.appendChild(div);
-                
+                    
           var LeagueName = document.createElement("div");
           LeagueName.classList.add("LeagueName");
           div.appendChild(LeagueName);
-                
+                    
           var timeBox = document.createElement("div");
           timeBox.classList.add("timeBox");
           div.appendChild(timeBox);
-                
+                    
           var fighterBox = document.createElement("div");
           fighterBox.classList.add("fighterBox");
           div.appendChild(fighterBox);
-                
+                    
           var names = document.createElement("div");
           names.classList.add("nameBox");
           div.appendChild(names);
-                    
+                        
           var homeFighterName = document.createElement("span");
           homeFighterName.classList.add("homeFighterName");
           names.appendChild(homeFighterName);
-                
+                    
           var awayFighterName = document.createElement("span");
           awayFighterName.classList.add("awayFighterName");
           names.appendChild(awayFighterName);
-                
+                    
           var homeFighterPicture = document.createElement("img");
           homeFighterPicture.src = homeFighterPictureData;
           homeFighterPicture.classList.add("homeFighterPicture");
           fighterBox.appendChild(homeFighterPicture);
-                
+                    
           var awayFighterPicture = document.createElement("img");
           awayFighterPicture.src = awayFighterPictureData;
           awayFighterPicture.classList.add("awayFighterPicture");
           fighterBox.appendChild(awayFighterPicture);
-                
                     
+                        
+                        
                     
-                
           LeagueName.innerHTML = data['DATA'][g]['NAME_PART_2'];
           homeFighterName.innerHTML = data['DATA'][g]['EVENTS'][i]['HOME_PARTICIPANT_NAME_ONE'];
           awayFighterName.innerHTML = data['DATA'][g]['EVENTS'][i]['AWAY_PARTICIPANT_NAME_ONE'];
           timeBox.innerHTML = ("Date: " + new Date((data['DATA'][g]['EVENTS'][i]['START_TIME']) * 1000));
-		  
-		}
-	}
+          
+        }
+      }
+	  });
+
+    
+  }
+  
+  
+  title = 'boxing_api';
+  
 		  
 }
